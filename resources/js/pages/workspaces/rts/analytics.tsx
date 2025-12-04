468977976b7b824b3051ac1233c2dc3a85bf341a
Kyle Anthony <kyleanthony47@gmail.com>
2025-12-04 13:50:49 +0800

    Adds RTS Analytics page with date filter
    Adds the initial implementation of the RTS Analytics page.
    
    Includes date filtering functionality and basic statistics cards.
    The statistics shown are RTS Rate, RTS Amount, Tracked Orders, and Parcel Updates Sent.
    Also includes required dependencies and radix ui components.

--- resources/js/pages/workspaces/rts/analytics.tsx
+++ resources/js/pages/workspaces/rts/analytics.tsx
@@ -1,9 +1,14 @@
 import AppLayout from '@/layouts/app-layout';
 import RtsNavigation from '@/pages/workspaces/rts/partials/RtsNavigation';
 import { Workspace } from '@/types/models/Workspace';
-import workspace from '@/routes/workspace';
-import { Card, CardDescription, CardHeader, CardTitle, CardContent, CardFooter } from '@/components/ui/card';
+import { Card, CardHeader, CardTitle, CardContent } from '@/components/ui/card';
 import { useMemo } from 'react';
+import React from 'react';
+import { Calendar } from '@/components/ui/calendar';
+import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/popover';
+import { CalendarIcon, FilterIcon } from 'lucide-react';
+import { Input } from '@/components/ui/input';
+import { Button } from '@/components/ui/button';
 
 type Props = {
     workspace: Workspace;
@@ -17,7 +22,27 @@ type Props = {
     }
 }
 
-const Analytics = ({ workspace, data } : Props) => {
+function formatDate(date: Date | undefined) {
+    if (!date) {
+        return ""
+    }
+
+    return date.toLocaleDateString("en-US", {
+        day: "2-digit",
+        month: "long",
+        year: "numeric",
+    })
+}
+
+function isValidDate(date: Date | undefined) {
+    if (!date) {
+        return false
+    }
+    return !isNaN(date.getTime())
+}
+
+
+const Analytics = ({ workspace, data }: Props) => {
     const analytics = useMemo(() => {
         return [
             { title: 'RTS Rate', value: `${data.rts_rate_percentage}%` },
@@ -27,10 +52,85 @@ const Analytics = ({ workspace, data } : Props) => {
         ]
     }, [data])
 
+    const [open, setOpen] = React.useState(false)
+    const [date, setDate] = React.useState<Date | undefined>(
+        new Date("2025-06-01")
+    )
+    const [month, setMonth] = React.useState<Date | undefined>(date)
+    const [value, setValue] = React.useState(formatDate(date))
     return (
         <AppLayout>
             <div className='px-4 py-6'>
-                <RtsNavigation workspace={workspace}/>
+                <div className='flex justify-between mb-5'>
+                    <h1 className="text-center text-4xl font-extrabold text-balance">
+                        Analytics
+                    </h1>
+
+                    <div>
+                        <div className='flex items-center gap-3'>
+                            <Button variant="secondary" size="sm">
+                                <FilterIcon className="mr-2 h-4 w-4" />
+                                Filters
+                            </Button>
+
+                            <div className="relative flex gap-2">
+                                <Input
+                                    id="date"
+                                    value={value}
+                                    placeholder="June 01, 2025"
+                                    className="bg-background pr-10"
+                                    onChange={(e) => {
+                                        const date = new Date(e.target.value)
+                                        setValue(e.target.value)
+                                        if (isValidDate(date)) {
+                                            setDate(date)
+                                            setMonth(date)
+                                        }
+                                    }}
+                                    onKeyDown={(e) => {
+                                        if (e.key === "ArrowDown") {
+                                            e.preventDefault()
+                                            setOpen(true)
+                                        }
+                                    }}
+                                />
+                                <Popover open={open} onOpenChange={setOpen}>
+                                    <PopoverTrigger asChild>
+                                        <Button
+                                            id="date-picker"
+                                            variant="ghost"
+                                            className="absolute top-1/2 right-2 size-6 -translate-y-1/2"
+                                        >
+                                            <CalendarIcon className="size-3.5" />
+                                            <span className="sr-only">Select date</span>
+                                        </Button>
+                                    </PopoverTrigger>
+                                    <PopoverContent
+                                        className="w-auto overflow-hidden p-0"
+                                        align="end"
+                                        alignOffset={-8}
+                                        sideOffset={10}
+                                    >
+                                        <Calendar
+                                            mode="single"
+                                            selected={date}
+                                            captionLayout="dropdown"
+                                            month={month}
+                                            onMonthChange={setMonth}
+                                            onSelect={(date) => {
+                                                setDate(date)
+                                                setValue(formatDate(date))
+                                                setOpen(false)
+                                            }}
+                                        />
+                                    </PopoverContent>
+                                </Popover>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <RtsNavigation workspace={workspace} />
 
                 <div className="grid grid-cols-4 gap-4">
                     {analytics.map((data, key) => {
