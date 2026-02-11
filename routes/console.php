725be17b2ffbe87ef00c6a4e05a647a3429fd618
Bryan Mulingbayan <bryan.mulingbayan96@gmail.com>
2026-02-11 11:40:07 +0800

    fix

--- routes/console.php
+++ routes/console.php
@@ -8,4 +8,5 @@ Artisan::command('inspire', function () {
 })->purpose('Display an inspiring quote');
 
 Schedule::command('trigger-fetch-page-orders')->hourlyAt(0);
-//Schedule::command('trigger-fetch-ads-data')->hourlyAt(30);
+Schedule::command('trigger-fetch-sequences')->weekly(1);
+Schedule::command('trigger-fetch-sequence-statistics')->weekly(1);
