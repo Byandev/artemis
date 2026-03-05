1006f51bb24e3cc6fd32885967386ba63765824c
Bryan Mulingbayan <bryan.mulingbayan96@gmail.com>
2026-03-05 22:16:00 +0800

    fix

---
+++ app/Console/Commands/TriggerFetchShopCustomers.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Shop;
+use Illuminate\Console\Command;
+use Modules\Pancake\Jobs\FetchShopCustomers;
+
+class TriggerFetchShopCustomers extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'trigger-fetch-shop-customers';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle(): void
+    {
+        Shop::all()
+            ->each(function (Shop $shop) {
+                dispatch(new FetchShopCustomers($shop, 1, \Carbon\Carbon::parse($shop->customers_last_synced_at)->unix(), \Carbon\Carbon::now()->unix()));
+            });
+    }
+}
