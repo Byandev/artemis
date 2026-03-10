24f765dae2a65588456f5c29b7c5f85b07ad4ded
Bryan Mulingbayan <bryan.mulingbayan96@gmail.com>
2026-03-10 09:15:03 +0800

    fix

--- Modules/Pancake/app/Jobs/FetchShopCustomers.php
+++ Modules/Pancake/app/Jobs/FetchShopCustomers.php
@@ -3,6 +3,7 @@
 namespace Modules\Pancake\Jobs;
 
 use App\Models\Shop;
+use Carbon\Carbon;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Modules\Pancake\Services\Pancake;
 
@@ -31,7 +32,7 @@ class FetchShopCustomers implements ShouldQueue
 
         $pancake = new Pancake($this->shop->id, $this->shop->pages->first()->pos_token);
 
-        $response = $pancake->listCustomers("&page_size=100&page_number=$page_number&start_time_updated_at=$this->startTime&end_time_updated_at=$this->endTime");
+        $response = $pancake->listCustomers("&page_size=250&page_number=$page_number&start_time_updated_at=$this->startTime&end_time_updated_at=$this->endTime");
 
         $totalPages = $response['total_pages'];
 
@@ -43,6 +44,8 @@ class FetchShopCustomers implements ShouldQueue
 
         if ($totalPages > $this->page_number) {
             dispatch(new FetchShopCustomers($this->shop, $page_number + 1, $this->startTime, $this->endTime))->delay(now()->addSeconds(5));
+        } else {
+            $this->shop->update(['customers_last_synced_at' => Carbon::createFromTimestamp($this->endTime)->subHours(8)]);
         }
     }
 }
