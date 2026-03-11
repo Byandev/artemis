dffa451b4972a79b7e03a938756c5dde5cc5176a
Bryan Mulingbayan <bryan.mulingbayan96@gmail.com>
2026-03-11 15:40:04 +0800

    fix

--- app/Metrics/Orders/TotalSales.php
+++ app/Metrics/Orders/TotalSales.php
@@ -30,18 +30,20 @@ final class TotalSales
     private function baseQuery(int $workspaceId, array $date_range, array $filter)
     {
         return DB::table('pancake_orders')
-            ->join('pages', 'pages.id', '=', 'pancake_orders.page_id')
             ->where('pancake_orders.workspace_id', $workspaceId)
             ->whereNotNull('pancake_orders.confirmed_at')
             ->whereBetween('pancake_orders.confirmed_at', [
                 $date_range['start_date'].' 00:00:00',
                 $date_range['end_date'].' 23:59:59',
             ])
-            ->when(isset($filter['page_ids']) && $filter['page_ids'], function ($query) use ($filter) {
-                $query->whereIn('pages.id', explode(',', $filter['page_ids']));
-            })
-            ->when(isset($filter['shop_ids']) && $filter['shop_ids'], function ($query) use ($filter) {
-                $query->whereIn('pages.shop_id', explode(',', $filter['shop_ids']));
+            ->when((isset($filter['page_ids']) && $filter['page_ids']) || (isset($filter['shop_ids']) && $filter['shop_ids']), function ($query) use ($filter) {
+                $query->join('pages', 'pages.id', '=', 'pancake_orders.page_id')
+                    ->when(isset($filter['page_ids']) && $filter['page_ids'], function ($query) use ($filter) {
+                        $query->whereIn('pages.id', explode(',', $filter['page_ids']));
+                    })
+                    ->when(isset($filter['shop_ids']) && $filter['shop_ids'], function ($query) use ($filter) {
+                        $query->whereIn('pages.shop_id', explode(',', $filter['shop_ids']));
+                    });
             })
             ->whereNotIn('pancake_orders.status', [6, 7]);
     }
