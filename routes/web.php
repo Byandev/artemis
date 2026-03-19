f0b51e223b8a2c728fe1057881ca4e0f813cf3f4
Neil Patrick Mulingbayan <neilpatrick.personal@gmail.com>
2026-03-19 15:50:50 +0800

    Adds CSR Leaderboards page
    Introduces the client-side implementation for the CSR Leaderboards page, accessible via `/leaderboards`.
    Develops React components for displaying top users with distinct cards and a detailed list for other participants.
    Includes initial styling and visual effects for an engaging user experience.
    Establishes basic sorting and data formatting logic, utilizing hardcoded sample data for initial development.
    Extracts a shared user type definition for improved consistency.

--- routes/web.php
+++ routes/web.php
@@ -7,10 +7,10 @@ Route::get('/', function () {
     return Inertia::render('welcome');
 })->name('home');
 
-Route::get('/csr-leaderboard', function () {
-    return redirect('/csr-leaderboards/index.html');
-})->name('csr-leaderboard.public');
 
+Route::get('/leaderboards', function () {
+    return Inertia::render('workspaces/public/leaderboard');
+});
 Route::get('/auth/facebook/callback', [\App\Http\Controllers\Integrations\FacebookController::class, 'callback']);
 
 Route::middleware(['auth'])->group(function () {
