f699d1bde581424ea04b4c98ce1aeadfd9341e29
Bryan Mulingbayan <bryan.mulingbayan96@gmail.com>
2026-03-26 14:19:36 +0800

    fix

--- resources/js/components/app-sidebar-header.tsx
+++ resources/js/components/app-sidebar-header.tsx
@@ -1,8 +1,5 @@
 import { SidebarTrigger } from '@/components/ui/sidebar';
 import { NavUser } from '@/components/nav-user';
-import { Button } from "@/components/ui/button"
-import { DropdownMenu, DropdownMenuContent, DropdownMenuTrigger } from "@/components/ui/dropdown-menu"
-import { Bell } from "lucide-react"
 import WorkspaceSwitcher from './workspace-switcher';
 import AppearanceToggleDropdown from '@/components/appearance-dropdown';
 
@@ -17,29 +14,6 @@ export function AppSidebarHeader() {
 
                 <div className="flex items-center gap-2 ml-auto">
                     <AppearanceToggleDropdown />
-                    <DropdownMenu>
-                        <DropdownMenuTrigger asChild>
-                            <Button variant="ghost" className="h-8 w-8 p-1 rounded-full bg-neutral-200 text-black dark:bg-neutral-700 dark:text-white">
-                                <Bell />
-                            </Button>
-                        </DropdownMenuTrigger>
-
-                        <DropdownMenuContent className="w-80">
-                            <div className="p-2 space-y-2">
-                                <p className="text-sm font-semibold">Notifications</p>
-
-                                <div className="text-sm">
-                                    <p>You have a new message</p>
-                                    <p className="text-muted-foreground text-xs">2 minutes ago</p>
-                                </div>
-
-                                <div className="text-sm">
-                                    <p>Your order has shipped</p>
-                                    <p className="text-muted-foreground text-xs">1 hour ago</p>
-                                </div>
-                            </div>
-                        </DropdownMenuContent>
-                    </DropdownMenu>
                     <NavUser />
                 </div>
             </div>
