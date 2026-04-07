388f44e70b0e480d5e05cdc31dece7235005c6ad
sleepdeprive-gray <ivan.gml097@gmail.com>
2026-04-07 09:39:24 +0800

    refactor(po-utils): remove unused buildPagination helper

--- resources/js/utils/purchased-orders.ts
+++ resources/js/utils/purchased-orders.ts
@@ -19,27 +19,6 @@ export const computeTotals = (cogAmount: string, deliveryFee: string) => {
     };
 };
 
-export const buildPagination = (currentPage: number, lastPage: number, perPage: number, totalRows: number) => {
-    const hasPrevious = currentPage > 1;
-    const hasNext = currentPage < lastPage;
-    const fromRow = totalRows === 0 ? 0 : Math.min((currentPage - 1) * perPage + 1, totalRows);
-    const toRow = totalRows === 0 ? 0 : Math.min(currentPage * perPage, totalRows);
-
-    const safeLast = Math.max(1, lastPage || 1);
-    const safeCurrent = Math.min(Math.max(currentPage, 1), safeLast);
-
-    const pages = safeLast <= 5
-        ? Array.from({ length: safeLast }, (_, i) => i + 1)
-        : (() => {
-            const start = Math.max(1, safeCurrent - 2);
-            const end = Math.min(safeLast, start + 4);
-            const adjustedStart = Math.max(1, end - 4);
-            return Array.from({ length: end - adjustedStart + 1 }, (_, i) => adjustedStart + i);
-        })();
-
-    return { hasPrevious, hasNext, fromRow, toRow, pages };
-};
-
 export const buildEmptyState = (
     rowsLength: number,
     loading: boolean,
