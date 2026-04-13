99d2c8950639d33dc95a2547c8f64bd57ebc41ed
Neil Patrick <neilpatrick.personal@gmail.com>
2026-04-13 11:01:55 +0800

    Schedule CSR ERP daily record fetching
    - Added the `trigger-fetch-csr-erp-dail-records` command to the console scheduler to run twice daily at 10:00 and 22:00.

--- routes/console.php
+++ routes/console.php
@@ -12,3 +12,5 @@ Schedule::command('trigger-fetch-page-orders')->hourlyAt(0);
 Schedule::command('save-parcel-journey-notification-log')->monthlyOn(14);
 Schedule::command('trigger-fetch-shops-customers')->hourlyAt(30);
 Schedule::command('trigger-fetch-shops-users')->daily(7);
+
+Schedule::command('trigger-fetch-csr-erp-dail-records')->twiceDaily(10, 22);
