95bd54ca4c3cd497e68ad85ab3ccd79be8ea5104
Bryan Mulingbayan <bryan.mulingbayan96@gmail.com>
2026-04-21 22:47:07 +0800

    WIP

--- routes/console.php
+++ routes/console.php
@@ -22,5 +22,6 @@ Schedule::command('trigger-fetch-csr-erp-dail-records')->dailyAt('12:00');
 Schedule::command('trigger-fetch-csr-erp-dail-records')->dailyAt('15:00');
 
 Schedule::command('sync:csr-daily-records')->dailyAt('03:00');
+Schedule::command('sync:csr-rmo-daily-records')->dailyAt('04:00');
 
 Schedule::command('trigger-fetch-pancake-user-engagements')->dailyAt('01:00');
