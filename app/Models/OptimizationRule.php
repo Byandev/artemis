4c08ca0164805d9da14c2b8a10de1b84a1e10d0a
sleepdeprive-gray <ivan.gml097@gmail.com>
2026-05-15 16:07:10 +0800

    feat: instrument OptimizationRule with LogsActivityForWorkspace trait

--- app/Models/OptimizationRule.php
+++ app/Models/OptimizationRule.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Traits\LogsActivityForWorkspace;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
@@ -9,6 +10,8 @@ use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class OptimizationRule extends Model
 {
+    use LogsActivityForWorkspace;
+
     protected $table = 'optimization_rules';
 
     protected $guarded = [];
