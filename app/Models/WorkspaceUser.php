bfb79c582fc730320b7829889a584c6686c68f63
sleepdeprive-gray <ivan.gml097@gmail.com>
2026-05-15 16:05:57 +0800

    feat: instrument WorkspaceUser with LogsActivityForWorkspace trait

--- app/Models/WorkspaceUser.php
+++ app/Models/WorkspaceUser.php
@@ -2,10 +2,13 @@
 
 namespace App\Models;
 
+use App\Traits\LogsActivityForWorkspace;
 use Illuminate\Database\Eloquent\Relations\Pivot;
 
 class WorkspaceUser extends Pivot
 {
+    use LogsActivityForWorkspace;
+
     protected $table = 'workspace_user';
 
     public $incrementing = true;
