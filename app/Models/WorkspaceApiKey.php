f0f36af9bc1b70f3cb969cb9d881f63119297019
sleepdeprive-gray <ivan.gml097@gmail.com>
2026-05-15 16:06:39 +0800

    feat: instrument WorkspaceApiKey with LogsActivityForWorkspace trait

--- app/Models/WorkspaceApiKey.php
+++ app/Models/WorkspaceApiKey.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Traits\LogsActivityForWorkspace;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
@@ -10,7 +11,7 @@ use Illuminate\Support\Str;
 
 class WorkspaceApiKey extends Model
 {
-    use HasFactory;
+    use HasFactory, LogsActivityForWorkspace;
 
     protected $fillable = ['workspace_id', 'name', 'key', 'key_encrypted', 'key_prefix', 'last_used_at'];
 
