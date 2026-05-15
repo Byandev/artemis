3e5a9285572c1e1d34d666d2ecc0864c0ae48083
sleepdeprive-gray <ivan.gml097@gmail.com>
2026-05-15 16:07:20 +0800

    feat: instrument OptimizationRuleCondition with LogsActivityForWorkspace trait

--- app/Models/OptimizationRuleCondition.php
+++ app/Models/OptimizationRuleCondition.php
@@ -2,11 +2,14 @@
 
 namespace App\Models;
 
+use App\Traits\LogsActivityForWorkspace;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class OptimizationRuleCondition extends Model
 {
+    use LogsActivityForWorkspace;
+
     protected $table = 'optimization_rule_conditions';
 
     protected $guarded = [];
