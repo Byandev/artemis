7c3ae2983555814803e0b43c612bd5d36ba24ffa
sleepdeprive-gray <ivan.gml097@gmail.com>
2026-05-19 17:50:57 +0800

    fix(routes): remove workspace admin support ticket routes

--- routes/workspaces.php
+++ routes/workspaces.php
@@ -5,7 +5,6 @@ use App\Http\Controllers\Admin\AdminSupportTicketController;
 use App\Http\Controllers\Admin\AdminWorkspaceController;
 use App\Http\Controllers\Workspaces\AdAccountController;
 use App\Http\Controllers\Workspaces\Admin\MetricSettingController;
-use App\Http\Controllers\Workspaces\Admin\SupportTicketAdminController;
 use App\Http\Controllers\Workspaces\AdsManager\AdController;
 use App\Http\Controllers\Workspaces\AdsManager\AdSetController;
 use App\Http\Controllers\Workspaces\AdsManager\CampaignController;
@@ -34,7 +33,6 @@ use App\Http\Controllers\Workspaces\WorkspaceController;
 use App\Http\Controllers\Workspaces\WorkspaceInvitationController;
 use App\Http\Controllers\Workspaces\WorkspaceMemberController;
 use App\Http\Controllers\Workspaces\WorkspaceSetupController;
-use App\Models\SupportTicket;
 use App\Models\Workspace;
 use Illuminate\Support\Facades\Route;
 use Modules\Botcake\Http\Controllers\Web\FlowController;
@@ -294,12 +292,6 @@ Route::middleware(['auth'])->group(function () {
     Route::post('/workspaces/{workspace:slug}/support', [SupportTicketController::class, 'store'])->name('support.store');
     Route::patch('/workspaces/{workspace:slug}/support/{ticket}', [SupportTicketController::class, 'update'])->name('support.update');
     Route::delete('/workspaces/{workspace:slug}/support/{ticket}', [SupportTicketController::class, 'destroy'])->name('support.destroy');
-    Route::get('/workspaces/{workspace:slug}/admin/support-tickets', [SupportTicketAdminController::class, 'index'])
-        ->name('admin.support-tickets.index')
-        ->can('viewAny', [SupportTicket::class, 'workspace']);
-    Route::patch('/workspaces/{workspace:slug}/admin/support-tickets/{ticket}', [SupportTicketAdminController::class, 'update'])
-        ->name('admin.support-tickets.update')
-        ->can('viewAny', [SupportTicket::class, 'workspace']);
 
 });
 
