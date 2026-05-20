2d25b223e3d89c3786746a2fae806dcbda942742
sleepdeprive-gray <ivan.gml097@gmail.com>
2026-05-20 12:13:46 +0800

    fix(permissions): resolve workspace admins as full access users

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -58,6 +58,9 @@ class HandleInertiaRequests extends Middleware
         $isOwner = $user && $workspaceModel
             ? $user->ownsWorkspace($workspaceModel)
             : false;
+        $isWorkspaceAdmin = $user && $workspaceModel
+            ? $user->isAdminOf($workspaceModel)
+            : false;
         $can = [
             'viewAnySupportTickets' => false,
         ];
@@ -107,6 +110,7 @@ class HandleInertiaRequests extends Middleware
                 'user' => $user ? array_merge($user->toArray(), [
                     'is_super_admin' => $user->isSuperAdmin(),
                     'is_workspace_owner' => $isOwner,
+                    'is_workspace_admin' => $isWorkspaceAdmin,
                     'is_csr' => $user && $workspaceModel ? $user->isCsrOf($workspaceModel) : false,
                     'permissions' => $permissions,
                     'can' => $can,
@@ -160,7 +164,7 @@ class HandleInertiaRequests extends Middleware
             return [];
         }
 
-        if ($user->ownsWorkspace($workspace)) {
+        if ($user->ownsWorkspace($workspace) || $user->isAdminOf($workspace)) {
             return ['*'];
         }
 
