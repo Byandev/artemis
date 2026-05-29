ba867ef19f6a1de2d8d41ea3753ac0f5ef02fda0
Bryan Mulingbayan <bryan.mulingbayan96@gmail.com>
2026-05-29 10:11:49 +0800

    fix

--- database/migrations/2026_05_28_120800_enable_teams_and_botcake_access.php
+++ database/migrations/2026_05_28_120800_enable_teams_and_botcake_access.php
@@ -7,12 +7,6 @@ return new class extends Migration
 {
     public function up(): void
     {
-        DB::table('workspaces')->update([
-            'teams_module_enabled' => true,
-            'botcake_module_enabled' => true,
-            'updated_at' => now(),
-        ]);
-
         $permissions = [
             'View Teams' => 'Teams',
             'View Botcake Sequences' => 'Botcake',
@@ -28,39 +22,6 @@ return new class extends Migration
                     'updated_at' => now(),
                 ],
             );
-        }
-
-        $permissionIds = DB::table('permissions')
-            ->whereIn('name', array_keys($permissions))
-            ->pluck('id');
-
-        if ($permissionIds->count() !== count($permissions)) {
-            return;
-        }
-
-        $roleIds = DB::table('roles')
-            ->leftJoin('role_permissions', 'roles.id', '=', 'role_permissions.role_id')
-            ->leftJoin('permissions', 'role_permissions.permission_id', '=', 'permissions.id')
-            ->where(function ($query) {
-                $query
-                    ->whereIn('roles.name', ['admin', 'owner'])
-                    ->orWhereIn('permissions.name', [
-                        'View Members',
-                        'View Roles',
-                        'View Pages',
-                        'View Shops',
-                    ]);
-            })
-            ->distinct()
-            ->pluck('roles.id');
-
-        foreach ($roleIds as $roleId) {
-            foreach ($permissionIds as $permissionId) {
-                DB::table('role_permissions')->updateOrInsert([
-                    'role_id' => $roleId,
-                    'permission_id' => $permissionId,
-                ]);
-            }
         }
     }
 
