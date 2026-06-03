b8ea5b6d126d7fecabcf6b385854e30c433883fd
Bryan Mulingbayan <bryan.mulingbayan96@gmail.com>
2026-06-03 16:03:15 +0800

    fix

--- app/Http/Controllers/PublicApi/RmoOrderController.php
+++ app/Http/Controllers/PublicApi/RmoOrderController.php
@@ -12,7 +12,6 @@ use Spatie\QueryBuilder\QueryBuilder;
 
 class RmoOrderController extends Controller
 {
-
     public function login(Request $request): JsonResponse
     {
         $query = User::whereRaw('LOWER(status) = ?', ['active']);
