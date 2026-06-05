2de1c7ba1bc060197373d7be0fccf04460b52fa3
Neil Patrick Mulingbayan <neilpatrick.personal@gmail.com>
2026-06-05 14:39:34 +0800

    wip

--- resources/js/pages/workspaces/rts/public-pages/rmo-management.tsx
+++ resources/js/pages/workspaces/rts/public-pages/rmo-management.tsx
@@ -812,7 +812,7 @@ export default function RmoManagement({
                                         key={i}
                                         className="text-[12px] leading-snug font-medium text-gray-800 dark:text-gray-200"
                                     >
-                                        {item.name}
+                                        *****
                                     </p>
                                 ))}
                             </div>
