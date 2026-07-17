b8ad990600f3d417d4619d72389a4778d93f06f6
Bryan Mulingbayan <bryan.mulingbayan96@gmail.com>
2026-07-17 11:28:05 +0800

    Inline page owner

--- routes/workspaces.php
+++ routes/workspaces.php
@@ -188,6 +188,7 @@ Route::middleware(['auth'])->group(function () {
     Route::get('/workspaces/{workspace}/pages/{page}/edit', [PageController::class, 'edit'])->name('workspaces.pages.edit');
     Route::put('/workspaces/{workspace}/pages/{page}', [PageController::class, 'update'])->name('workspaces.pages.update');
     Route::put('/workspaces/{workspace}/pages/{page}/budget', [PageController::class, 'updateBudget'])->name('workspaces.pages.update-budget');
+    Route::patch('/workspaces/{workspace}/pages/{page}/assign-owner', [PageController::class, 'assignOwner'])->name('workspaces.pages.assign-owner');
     Route::post('/workspaces/{workspace}/pages/{page}/archive', [PageController::class, 'archive'])->name('workspaces.pages.archive');
     Route::post('/workspaces/{workspace}/pages/{page}/restore', [PageController::class, 'restore'])->name('workspaces.pages.restore');
 
