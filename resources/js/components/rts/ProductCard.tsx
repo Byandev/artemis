c5fbb2c26d136447989ea10fa34886f7f4fae9e7
Bryan Mulingbayan <bryan.mulingbayan96@gmail.com>
2026-08-25 19:15:02 +0800

    Revert "Fix"
    This reverts commit d64a16a09978c759d6fa3cc63f6d885e180e1e8c.

--- resources/js/components/rts/ProductCard.tsx
+++ resources/js/components/rts/ProductCard.tsx
@@ -64,7 +64,7 @@ export default function ProductCard({
                 ),
                 cell: ({ row }) =>
                     row.original.item_name ? (
-                        'Product ######'
+                        row.original.item_name
                     ) : (
                         <span className="text-gray-400">Unknown</span>
                     ),
