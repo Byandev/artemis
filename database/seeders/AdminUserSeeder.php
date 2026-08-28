36669d713cee2a72970b1cb63cc65ef12bf0f12e
Neil Patrick Mulingbayan <neilpatrick.personal@gmail.com>
2026-08-28 19:20:13 +0800

    add seeder for finace account super admin

--- database/seeders/AdminUserSeeder.php
+++ database/seeders/AdminUserSeeder.php
@@ -21,5 +21,15 @@ class AdminUserSeeder extends Seeder
                 'email_verified_at' => now(),
             ]
         );
+
+        User::firstOrCreate(
+            ['email' => 'finance@artemis.ph'],
+            [
+                'name' => 'Finance Admin',
+                'password' => bcrypt('password123'),
+                'is_super_admin' => true,
+                'email_verified_at' => now(),
+            ]
+        );
     }
 }
